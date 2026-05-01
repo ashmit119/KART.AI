473c8c96b6566e3c4d19233ed94cc72b9cfd09de
ashmit119 <ashmitbanerjee11.pkt@gmail.com>
2026-05-01 06:25:35 +0530

    Cleaned up Vite config and added TanStack Router plugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,20 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import tailwindcss from '@tailwindcss/vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import { nitro } from 'nitro/vite'
 import path from "path"
 
 export default defineConfig({
     plugins: [
+        tanstackRouter(),
         tanstackStart(),
         tailwindcss(),
         react(),
         tsconfigPaths(),
-        nitro({
-            preset: 'vercel'
-        })
     ],
     resolve: {
         alias: {
